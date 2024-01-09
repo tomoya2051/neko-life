69c45a9b09135034ce0842d792b7b6696c6091e6
tomoya2051 <f.tomoya_2051@icloud.com>
2024-01-09 03:06:30 +0000

    viewファイルの編集

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
+    @diaries=Diary.all.order(created_at: :desc).limit(4)
   end
   
   def about
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -8,7 +8,7 @@ class Public::SessionsController < Devise::SessionsController
   end
 
   def after_sign_out_path_for(resource)
-    new_customer_registration_path
+    new_customer_session_path
   end
   # before_action :configure_sign_in_params, only: [:create]
 
